0bf6ed9dc4ec8ff7e898056b7d6fb72b879cfc04
Lora-Yagnasree <yagnasreekella20@gmail.com>
2025-06-17 15:25:17 +0530

    404

--- muster/settings.py
+++ muster/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7+@so6jr6bfoooz!r_59@kotha102v25qh)%#fp3l*$uf%st*+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -148,7 +148,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- muster/urls.py
+++ muster/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import handler404
+from django.shortcuts import render
  
  
 
@@ -30,3 +32,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
+def custom_404_view(request, exception):
+    return render(request, '404.html', status=404)
+
+handler404 = custom_404_view
